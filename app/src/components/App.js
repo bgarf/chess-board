c89f6fdeb6a5fdb955cd258e0ecc847170785c38
bethanie.garfin <bethanie.garfin@autotrader.co.uk>
2020-03-29 19:28:38 +0100

    Set up for react app

---
+++ app/src/components/App.js
@@ -0,0 +1,18 @@
+const App = () => {
+    return <Board />
+}
+
+// const root = document.getElementById('root')
+// const sqNumbers = []
+// for(let i = 1; i <= 64; i++) {sqNumbers.push(i)}
+// const squares = squares.map((value, index) => {
+//             <div key="{index}" className="{value}"/>
+//         })
+// console.log(squares)
+// const board = (
+//     <div id="board">
+//         {squares.map((value, index) => {
+//             <div key="{index}" className="{value}"/>
+//         })}
+//     </div>
+// )
